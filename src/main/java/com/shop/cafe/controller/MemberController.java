b7395967a2b9f4a986f0a52a4f52e68d63775d5b
Hann <Hann@host.docker.internal>
2025-03-06 16:54:12 +0900

    login, logout, Cart 세션 확인

--- src/main/java/com/shop/cafe/controller/MemberController.java
+++ src/main/java/com/shop/cafe/controller/MemberController.java
@@ -13,12 +13,38 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shop.cafe.dto.Member;
 import com.shop.cafe.service.MemberService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @RestController
-@CrossOrigin("http://127.0.0.1:5500/")
+//@CrossOrigin("http://127.0.0.1:8080/")
 public class MemberController {
 	
 	@Autowired
 	MemberService memberService;
+	
+	@PostMapping("login")
+	public Map<String, String> login(@RequestBody Member m, HttpServletRequest request) {
+		System.out.println(m);
+		Map<String, String> responseData = new HashMap();
+		try {
+			m = memberService.login(m);
+			if(m != null) { //login ok 로그인 성공
+				HttpSession session = request.getSession();
+				System.out.println(session.getId());
+				session.setAttribute("member", m);
+				responseData.put("msg", "ok");
+			}
+			else { //login fail 로그인 실패
+				responseData.put("msg", "다시 로그인 해주세요");
+			}
+		} 
+		catch (Exception e) { //login error 로그인 하다가 에러
+			e.printStackTrace();
+			responseData.put("msg", "다시 로그인 해주세요");
+		}
+		return responseData;
+	} //Post할 때만 Request 가능. GetMapping은 불가능.
 	
 	@PostMapping("insertMember")
 	public Map<String, String> insertMember(@RequestBody Member m) {
